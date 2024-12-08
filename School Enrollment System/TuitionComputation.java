2bc947027163206e193b2fbaf446e221e61108bb
ChewPuffy <121088243+ChewPuffy@users.noreply.github.com>
2024-12-08 17:59:05 +0800

    Update TuitionComputation.java

--- School Enrollment System/TuitionComputation.java
+++ School Enrollment System/TuitionComputation.java
@@ -1,3 +1,19 @@
 public class TuitionComputation extends CourseUNITS{
     
+    public int BSITRegularTuition(int discount){
+       return BSITunits * 1000 - discount;  
+    }
+
+    public int BMMARegularTuition(int discount){
+       return BMMAunits * 1000 - discount;  
+    }
+
+    public int BSTMRegularTuition(int discount){
+       return BSTMunits * 1000 - discount;  
+    }
+    
+    public int BSBARegularTuition(int discount){
+       return BSBAunits * 1000 - discount;  
+    }
+
 }
