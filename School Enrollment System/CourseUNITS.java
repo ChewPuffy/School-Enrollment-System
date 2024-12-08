ca34bf71bf60d2b37c9a941e6174edea3ee756b6
ChewPuffy <121088243+ChewPuffy@users.noreply.github.com>
2024-12-08 17:59:21 +0800

    Update CourseUNITS.java

--- School Enrollment System/CourseUNITS.java
+++ School Enrollment System/CourseUNITS.java
@@ -1,4 +1,126 @@
-public class CourseUNITS {
+import java.util.ArrayList;
+import java.util.Arrays;
 
-    
+public class CourseUNITS {
+int BSITunits = 23;
+int BMMAunits = 17;
+int BSTMunits = 20;
+int BSBAunits = 17;
+
+//First sem BSIT subjects
+ArrayList<String> BSITmajorSubs1stSem = new ArrayList<>(Arrays.asList(
+    "Data Structures and Algorithms",
+    "Human-Computer Interaction",
+    "Platform Technology (Operating Systems)"
+));
+
+ArrayList<String> BSITminorSubs1stSem = new ArrayList<>(Arrays.asList(
+    "Readings in Philippine History",
+    "Rizal's Life and Works",
+    "P.E./PATHFIT 3: Individual-Dual Sports",
+    "IT Elective 1",
+    "Principles of Communication"
+));
+
+//Second sem BSIT subjects
+ArrayList<String> BSITmajorSubs2ndSem = new ArrayList<>(Arrays.asList(
+    "Information Management",
+    "Network Technology 1",
+    "Quantitative Methods",
+    "Systems Integration and Architecture",
+    "Integrative Programming"
+));
+
+ArrayList<String> BSITminorSubs2ndSem = new ArrayList<>(Arrays.asList(
+    "The Entrepreneurial Mind",
+    "P.E./PATHFIT 4: Team Sports",
+    "Technopreneurship"
+));
+
+
+
+//First sem BMMA subjects
+ArrayList<String> BMMAmajorSubs1stSem = new ArrayList<>(Arrays.asList(
+    "2D Animation",
+    "3D Modeling",
+    "Color Theory"
+));
+
+ArrayList<String> BMMAminorSubs1stSem = new ArrayList<>(Arrays.asList(
+    "The Entrepreneurial Mind",
+    "Rizal's Life and Works",
+    "P.E./PATHFIT 3: Individual-Dual Sports"
+));
+
+//Second sem BMMA subjects
+ArrayList<String> BMMAmajorSubs2ndSem = new ArrayList<>(Arrays.asList(
+    "3D Animation",
+    "MMA Elective 1",
+    "Writing for New Media"
+));
+
+ArrayList<String> BMMAminorSubs2ndSem = new ArrayList<>(Arrays.asList(
+    "Art Appreciation",
+    "Philippine Popular Culture",
+    "P.E./PATHFIT 4: Team Sports"
+));
+
+
+
+//First sem BSTM subjects
+ArrayList<String> BSTMmajorSubs1stSem = new ArrayList<>(Arrays.asList(
+    "Quality Service Management in Tourism and Hospitality",
+    "Accommodation Operations and Management",
+    "Tour and Travel Management",
+    "Sustainable Tourism"
+));
+
+ArrayList<String> BSTMminorSubs1stSem = new ArrayList<>(Arrays.asList(
+    "Art Appreciation",
+    "P.E./PATHFIT 3: Individual-Dual Sports",
+    "Foreign Language 1"
+));
+
+//Second sem BSTM subjects
+ArrayList<String> BSTMmajorSubs2ndSem = new ArrayList<>(Arrays.asList(
+    "Tour Planning, Packaging, and Pricing",
+    "Tourism Policy Planning and Development",
+    "Introduction to Meetings, Incentives, Conferences, and Events (MICE)"
+));
+
+ArrayList<String> BSTMminorSubs2ndSem = new ArrayList<>(Arrays.asList(
+    "Ethics",
+    "P.E./PATHFIT 4: Team Sports",
+    "Science, Technology, and Society",
+    "Foreign Language 2"
+));
+
+
+
+
+//First sem BSBA subjects
+ArrayList<String> BSBAmajorSubs1stSem = new ArrayList<>(Arrays.asList(
+    "Costing and Pricing",
+    "Facilities Management"
+));
+
+ArrayList<String> BSBAminorSubs1stSem = new ArrayList<>(Arrays.asList(
+    "Philippine Popular Culture",
+    "Rizal's Life and Works",
+    "Purposive Communication",
+    "P.E./PATHFIT 3: Individual-Dual Sports"
+));
+
+//Second sem BSBA subjects
+ArrayList<String> BSBAmajorSubs2ndSem = new ArrayList<>(Arrays.asList(
+    "Business Law (Obligations and Contracts)",
+    "Taxation (Income Taxation)",
+    "Logistics Management"
+));
+
+ArrayList<String> BSBAminorSubs2ndSem = new ArrayList<>(Arrays.asList(
+    "The Entrepreneurial Mind",
+    "Art Appreciation",
+    "P.E./PATHFIT 4: Team Sports"
+));
 }
